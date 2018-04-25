edb6f2bb62d685ade68bd43517007ce58911d368
Kimmy :D <kimvg2@hotmail.com>
2018-04-25 13:30:51 +0200

    Tosti
    Changes text

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-    res.json({'msg': 'Hello you all!'})
+    res.json({'msg': 'I eat tosti'})
 });
 
 app.listen(8080, () => {
