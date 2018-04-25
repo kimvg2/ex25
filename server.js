0f0bebc9c520bd54e70ae6a20d94b6d8c3742a86
Kimmy :D <kimvg2@hotmail.com>
2018-04-25 13:33:31 +0200

    nomnom

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-    res.json({'msg': 'I eat tosti'})
+    res.json({'msg': 'I eat tosti. nomnom'})
 });
 
 app.listen(8080, () => {
